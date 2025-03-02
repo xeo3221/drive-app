bc59b401269cd7ee7c3f3c053a29fe53571654b6
xeo3221 <95309495+xeo3221@users.noreply.github.com>
2025-03-02 21:10:36 +0100

    posthog fix

--- next.config.js
+++ next.config.js
@@ -15,25 +15,6 @@ const config = {
   },
 
   // next.config.js
-
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
-    ];
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
 };
 
 export default config;
