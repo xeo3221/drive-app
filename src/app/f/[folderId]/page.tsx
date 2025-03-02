b6affd5197ed06583195124b281b1e7feb4d5528
xeo3221 <95309495+xeo3221@users.noreply.github.com>
2025-03-02 02:31:05 +0100

    Cleanup with minimal data access layer

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,9 +1,5 @@
 import DriveContents from "../../drive-contents";
-import {
-  getAllParentsForFolder,
-  getFiles,
-  getFolders,
-} from "~/server/db/queries";
+import { QUERIES } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
@@ -16,9 +12,9 @@ export default async function GoogleDriveClone(props: {
   }
 
   const [folders, files, parents] = await Promise.all([
-    getFolders(parsedFolderId),
-    getFiles(parsedFolderId),
-    getAllParentsForFolder(parsedFolderId),
+    QUERIES.getFolders(parsedFolderId),
+    QUERIES.getFiles(parsedFolderId),
+    QUERIES.getAllParentsForFolder(parsedFolderId),
   ]);
   return <DriveContents files={files} folders={folders} parents={parents} />;
 }
--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,3 +1,5 @@
+import "server-only";
+
 import { db } from "~/server/db";
 import {
   files_table as filesSchema,
@@ -5,33 +7,37 @@ import {
 } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function getAllParentsForFolder(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
-
-  while (currentId !== null) {
-    const folder = await db
+export const QUERIES = {
+  getFolders: function (folderId: number) {
+    return db
       .select()
       .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
+      .where(eq(foldersSchema.parent, folderId));
+  },
+  getFiles: function (folderId: number) {
+    return db
+      .select()
+      .from(filesSchema)
+      .where(eq(filesSchema.parent, folderId));
+  },
+  getAllParentsForFolder: async function (folderId: number) {
+    const parents = [];
+    let currentId: number | null = folderId;
 
-    if (!folder[0]) {
-      // throw new Error("Parent folder not found");
-      break;
+    while (currentId !== null) {
+      const folder = await db
+        .select()
+        .from(foldersSchema)
+        .where(eq(foldersSchema.id, currentId));
+
+      if (!folder[0]) {
+        // throw new Error("Parent folder not found");
+        break;
+      }
+      parents.unshift(folder[0]);
+
+      currentId = folder[0].parent;
     }
-    parents.unshift(folder[0]);
-
-    currentId = folder[0].parent;
-  }
-  return parents;
-}
-
-export function getFolders(folderId: number) {
-  return db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, folderId));
-}
-export function getFiles(folderId: number) {
-  return db.select().from(filesSchema).where(eq(filesSchema.parent, folderId));
-}
+    return parents;
+  },
+};
